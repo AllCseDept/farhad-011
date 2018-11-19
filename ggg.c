d088635c1fe03cb8847182974c175521151b1b61
farhad1205 <44949302+farhad1205@users.noreply.github.com>
2018-11-19 15:36:43 +0600

    Add files via upload

---
+++ ggg.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,p;
+    printf("Enter one number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    printf("Enter a choice: ");
+    scanf("%d",&p);
+    for(p=1;p<1;p++)
+   {
+
+       p=a+b;
+
+
+      printf("%d",p);
+   }
+    for(p=2;p<2;p++)
+   {
+
+       p=a-b;
+
+
+      printf("%d",p);
+   }
+
+    return 0;
+}
